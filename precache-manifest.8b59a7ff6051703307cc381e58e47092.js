fc7684fd91b300e73babed01a8f733b005c429e9
u4bi <rootcode10@gmail.com>
2019-11-06 14:36:42 +0900

    chore: github pages에 배포

---
+++ precache-manifest.8b59a7ff6051703307cc381e58e47092.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5507233c0bce6212d879d45cf84be70d",
+    "url": "/bingo/index.html"
+  },
+  {
+    "revision": "58fdda2fd252247450ad",
+    "url": "/bingo/static/css/main.29f08483.chunk.css"
+  },
+  {
+    "revision": "58b7fef07e9ac458cfaf",
+    "url": "/bingo/static/js/2.d0c81f4f.chunk.js"
+  },
+  {
+    "revision": "58fdda2fd252247450ad",
+    "url": "/bingo/static/js/main.94747a67.chunk.js"
+  },
+  {
+    "revision": "88591175206a26be6f19",
+    "url": "/bingo/static/js/runtime-main.8bc20d01.js"
+  }
+]);
